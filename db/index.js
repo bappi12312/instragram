c97b43266c8dc5874b4204c7d354ae9176dc7031
bappi12312 <ranamdbappihossain@gmail.com>
2024-08-13 21:11:10 +0600

    server ready and mongodb connected

---
+++ db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}}`)
+
+    console.log(`MongoDB connected !! DB host : ${connectInstance.connection.host}`)
+  } catch (error) {
+    console.log("mongodb error :", error);
+    process.exit(1);
+  }
+}
