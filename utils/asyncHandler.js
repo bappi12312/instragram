05da7e713941432d75e25cd262f0511242e884c0
bappi12312 <ranamdbappihossain@gmail.com>
2024-08-13 21:29:07 +0600

    async handler added

---
+++ utils/asyncHandler.js
@@ -0,0 +1,18 @@
+const asyncHandler = (handler) => {
+  return (req,res,next) => {
+    Promise.resolve(handler(req,res,next)).catch((err) => next(err))
+  }
+}
+
+export {asyncHandler}
+
+// const asyncHandler = (fn) => async (req,res,next) => {
+//   try {
+//     await fn(req,res,next)
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message
+//     })
+//   }
+// }
